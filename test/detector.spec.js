b2ecfc7bc18e50c17252a2a2817bdc802833a068
Justin J. Moses <justinjmoses@gmail.com>
2014-10-15 13:52:59 -0400

    Adding ignore patterns

--- test/detector.spec.js
+++ test/detector.spec.js
@@ -4,18 +4,18 @@ var expect = require('chai').expect;
 var path = require('path');
 var detector = require('../lib/index');
 
-function makePath() {
-    return path.join.apply(null, arguments);
-}
-
 describe('detector', function () {
-    it('finds invalid paths that are case sensitive', function () {
-        var invalid = detector([
-            makePath('test', 'harness', 'folderA', 'fileA.js'),
-            makePath('test', 'harness', 'folderA', 'fileB.js'),
-            makePath('test', 'harness', 'folderA', 'fileC.js'),
-        ]);
-        expect(invalid.length).to.equal(1);
-        expect(invalid[0].path).to.equal('./fileb');
+    it('finds invalid paths that are case sensitive', function (done) {
+        var invalid = detector('./test/harness', {}, function (invalid) {
+            expect(invalid.length).to.equal(1);
+            expect(invalid[0].path).to.equal('./fileb');
+            done();
+        });
+    });
+    it('ignores patterns', function (done) {
+        var invalid = detector('./test/harness', { ignore: ['folderA'] }, function (invalid) {
+            expect(invalid.length).to.equal(0);
+            done();
+        });
     });
 });
