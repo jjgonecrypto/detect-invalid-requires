10c64445c5c5aff574c43c07d899feb9228f085e
Justin J. Moses <justinjmoses@gmail.com>
2014-10-17 14:33:01 -0400

    Updating readme and linting tests

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-Simple utility to detect invalid modular requires that do not match in a case sensitive environment. Useful when developers are all on case insensitive dev machines (OSX and Windows), yet CI server is Linux.
+Simple utility to detect invalid relative require paths that do not exist.
+It is case-sensitive, so in particular it is useful when developers are all on
+case insensitive dev machines (OSX and Windows), yet CI server is Linux.
 
 Usage
 ===
 
     npm install -g detect-invalid-requires
-    detect-invalid-requires (files)
+
+    detect-invalid-requires [options] (files)
+
+Options
+=====
+
+`-i, --ignore list` Comma delimited list of path strings to ignore
--- test/detector.spec.js
+++ test/detector.spec.js
@@ -1,19 +1,18 @@
 'use strict';
 
 var expect = require('chai').expect;
-var path = require('path');
 var detector = require('../lib/index');
 
 describe('detector', function () {
     it('finds invalid paths that are case sensitive', function (done) {
-        var invalid = detector('./test/harness', {}, function (invalid) {
+        detector('./test/harness', {}, function (invalid) {
             expect(invalid.length).to.equal(1);
             expect(invalid[0].path).to.equal('./fileb');
             done();
         });
     });
     it('ignores patterns', function (done) {
-        var invalid = detector('./test/harness', { ignore: ['folderA'] }, function (invalid) {
+        detector('./test/harness', { ignore: ['folderA'] }, function (invalid) {
             expect(invalid.length).to.equal(0);
             done();
         });
